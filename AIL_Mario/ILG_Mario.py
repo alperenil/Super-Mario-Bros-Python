2d78a4b4a95d4b1dc94cecd538a84d03fc84e1d8
wholelottabs <imdoingmy.partffs@gmail.com>
2023-12-29 15:22:01 +0100

    Comments

--- AIL_Mario/ILG_Mario.py
+++ AIL_Mario/ILG_Mario.py
@@ -1,6 +1,5 @@
 # Importierte Bibliotheken
 import pygame
-import os
 # import random
 # import math
 from os import listdir
@@ -22,7 +21,7 @@ PLAYER_VEL = 5
 GRAVITY = 1
 ANIMATION_DELAY = 3
 
-# Definiert Fenster
+# Definiert Fenster in Variable window
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 '''
@@ -35,80 +34,86 @@ def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
 
-# Funktion ladet Sprites
+# Funktion holt gewünschte Sprites
 def load_sprite_sheets(dir1, dir2, width, height, direction=False):
-    path = join("assets", dir1, dir2)
-    images = [f for f in listdir(path) if isfile(join(path, f))]  # Ladet alle Files in der Directory
+    path = join("assets", dir1, dir2)  # Variable für den Pfad des gewünschten Sprites
+    images = [f for f in listdir(path) if isfile(join(path, f))]  # Liste enthält alle Bilder aus dem Pfad
 
     all_sprites = {}
 
-    for image in images:
-        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()  # Ladet alle Bilder
+    for image in images:  # Für jedes Bild in images liste
+        # Ladet alle Bilder in sprite_sheet mit durchsichtigem hintergrund
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
 
         sprites = []
+
+        # Teilt Bilder mit mehreren Sprites in einzelne Bilder auf
         for i in range(sprite_sheet.get_width() // width):  # Teilt Bilder auf
-            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-            rect = pygame.Rect(i * width, 0, width, height)
-            surface.blit(sprite_sheet, (0, 0), rect)
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)  # Definiert grösse von einzelnen Bildern
+            rect = pygame.Rect(i * width, 0, width, height)  # setzt grösse rect fest für die einzelnen Bilder
+            surface.blit(sprite_sheet, (0, 0), rect)  # Zeichnet die einzelnen Bilder
             sprites.append(pygame.transform.scale2x(surface))  # Skaliert einzelne Bilder
 
-        if direction:  # Nimmt .png aus den Namen und gibt nach richtung anderen Namen
+        # Wenn eine Richtung gegeben ist, wird hier das ".png" aus dem Namen entfernt und die Richtung als suffix
+        # angefügt. Schliesslich werden die Sprites je nach Richtung umgedreht und in die Variable hinzugefügt.
+        if direction:
             all_sprites[image.replace(".png", "") + "_right"] = sprites
             all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
         else:
             all_sprites[image.replace(".png", "")] = sprites
 
+    # Gibt die Gedrehten und umbenannten Sprites wieder zurück
     return all_sprites
 
 
-# Holt gewünschten Block
+# Funktion holt gewünschten Block
 def get_block(size):
-    path = join("assets", "Terrain", "Ground_Brown.png")
-    image = pygame.image.load(path).convert_alpha()
+    path = join("assets", "Terrain", "Ground_Brown.png")  # Definiert Pfad des gewünschten Blocks
+    image = pygame.image.load(path).convert_alpha()  # Holt Bild aus dem Pfad und gibt durchsichtigen Hintergrund
     surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)  # Erstellt Image mit der gegebenen Size
     rect = pygame.Rect(0, 0, size, size)  # Holt die richtigen Pixel aus der Bilddatei
     surface.blit(image, (0, 0), rect)  # Zeichnet Block
-    return pygame.transform.scale2x(surface)  # Verdoppelt Grösse
+    return pygame.transform.scale2x(surface)  # Skaliert Block
 
 
 # Klasse für den Spieler
 class Player(pygame.sprite.Sprite):
-    COLOR = (255, 0, 0)
+    # Ruft Funktion auf, um Charakter Sprites zu holen
     SPRITES = load_sprite_sheets("MainCharacters", "Mario", 32, 32, True)
 
     # Initialisierung des Spielers
     def __init__(self, x, y, width, height):
         super().__init__()
-        self.rect = pygame.Rect(x, y, width, height)
-        self.x_vel = 0
-        self.y_vel = 0
-        self.mask = None
-        self.direction = "right"
-        self.animation_count = 0
-        self.fall_count = 0
-        self.jump_count = 0
+        self.rect = pygame.Rect(x, y, width, height)  # Grösse und position des Spielers
+        self.x_vel = 0  # Geschwindigkeit x-Achse
+        self.y_vel = 0  # Geschwindigkeit y-Achse
+        self.mask = None  # Maske für kollisionserkennung
+        self.direction = "right"  # Richtung, in die der Spieler aktuell schaut
+        self.animation_count = 0  # Counter für animationen
+        self.fall_count = 0  # Counter für Fallzeit
+        self.jump_count = 0  # Counter für Anzahl Sprünge
 
     # Methode sorgt für Sprung
     def jump(self):
-        self.y_vel = -GRAVITY * 8
-        self.animation_count = 0
-        self.jump_count += 1
+        self.y_vel = -GRAVITY * 8  # Gibt Geschwindigkeit nach oben (Dynamisch mit Gravitation)
+        self.animation_count = 0  # Setzt animations counter zurück
+        self.jump_count += 1  # zähl Sprünge hoch
         if self.jump_count == 1:
             self.fall_count = 0
 
-    # Move funktion für die bewegung des Spielers
+    # Methode bewegt Spieler (Für Gravitation und Kollisionserkennung)
     def move(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
+        self.rect.x += dx  # Bewegt Spieler in x Richtung
+        self.rect.y += dy  # Bewegt Spieler in y Richtung
 
-    # Bewegt Spieler nach Links
+    # Bewegt Spieler nach links (Für Auswirkungen von Inputs)
     def move_left(self, vel):
         self.x_vel = -vel
         if self.direction != "left":
             self.direction = "left"
             self.animation_count = 0
 
-    # Bewegt Spieler nach Rechts
+    # Bewegt Spieler nach rechts (Für Auswirkungen von Inputs)
     def move_right(self, vel):
         self.x_vel = vel
         if self.direction != "right":
@@ -150,10 +155,10 @@ class Player(pygame.sprite.Sprite):
         self.animation_count += 1
         self.update()
 
-    # Passt "Hitbox" an Sprite an
+    # Passt Hit-box an Sprite an
     def update(self):
         self.rect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
-        self.mask = pygame.mask.from_surface(self.sprite)  # Sucht einzelne Pixel raus für Pixelperfekte Kollision
+        self.mask = pygame.mask.from_surface(self.sprite)  # Sucht einzelne Pixel raus für Pixel perfekte Kollision
 
     # Zeichnet Spieler
     def draw(self, win, offset_x):
@@ -171,8 +176,8 @@ class Object(pygame.sprite.Sprite):
         self.name = name
 
     # Zeichnet Objekte
-    def draw(self, window, offset_x):
-        window.blit(self.image, (self.rect.x - offset_x, self.rect.y))
+    def draw(self, win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
 
 # Klasse für Blöcke
@@ -190,10 +195,12 @@ def handle_vertical_collision(player, objects, dy):
     for obj in objects:
         if pygame.sprite.collide_mask(player, obj):  # Kollision wird hier für alle Objekte berechnet mithilfe der maske
             if dy > 0:  # Prüft, ob Spieler von oben kollidiert
-                player.rect.bottom = obj.rect.top + 2  # Setzt untere Seite vom Spieler gleich mit oberer Seite von Objekt
+                # Setzt untere Seite vom Spieler gleich mit oberer Seite von Objekt
+                player.rect.bottom = obj.rect.top + 2
                 player.landed()  # Ruft Methode landed auf
             elif dy < 0:  # Prüft, ob Spieler von unten kollidiert
-                player.rect.top = obj.rect.bottom - 2  # Setzt obere Seite vom Spieler gleich mit unterer Seite von Objekt
+                # Setzt obere Seite vom Spieler gleich mit unterer Seite von Objekt
+                player.rect.top = obj.rect.bottom - 2
                 player.hit_head()  # Ruft Methode hit_head auf
 
         collided_objects.append(obj)  # Fügt Objekt zu collided_objects hinzu
@@ -207,7 +214,7 @@ def collide(player, objects, dx):
     player.update()  # Updated Masken
     collided_object = None
     for obj in objects:
-        if pygame.sprite.collide_mask(player, obj):  # Setzt Collided object, wenn kollision entdetckt wird
+        if pygame.sprite.collide_mask(player, obj):  # Setzt Collided object, wenn kollision entdeckt wird
             collided_object = obj
             break
 
@@ -235,8 +242,8 @@ def handle_move(player, objects):
 
 
 # Funktion "zeichnet" Hintergrund und Spieler bei Aufruf
-def draw(window, player, objects, offset_x):
-    window.fill(BACKGROUND_COLOR)
+def draw(win, player, objects, offset_x):
+    win.fill(BACKGROUND_COLOR)
 
     for obj in objects:
         obj.draw(window, offset_x)
